13c2bac5569724c349f976443cebd0e2f221e7e0
Neto Leal <neto.leal@loft.com.br>
2020-07-18 00:26:54 -0300

    card ready

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,64 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('images/avatar.jpg'),
+          ),
+          Text(
+            'Neto Leal',
+            style: TextStyle(
+                fontSize: 40,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico'),
+          ),
+          Text('CREATIVE DEVELOPER',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.teal.shade100,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Source Sans Pro',
+              )),
+          SizedBox(
+            height: 30,
+            width: 150,
+            child: Divider(
+              color: Colors.teal.shade100,
             ),
-            Container(
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.green,
-                    )
-                  ],
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
                 ),
-              ),
-            ),
-            Container(
-              color: Colors.blue,
-              width: 100,
-            )
-          ],
-        ),
-      ),
+                title: Text('+55 11 98727 2987',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                    ))),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 0, horizontal: 25),
+            child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text('netoleal@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                    ))),
+          ),
+        ],
+      )),
     ));
   }
 }
